37d8b9f3199faef5cbe0ad33e72d1c63632698a1
Kosuke Maeda <maeda.korosuke@gmail.com>
2016-10-03 14:38:19 +0900

    Firebaseサンプルアプリを参考にコードの整理をした

--- app/src/main/java/maeda/edu/realtimedatabasesample/MainActivity.java
+++ app/src/main/java/maeda/edu/realtimedatabasesample/MainActivity.java
@@ -15,31 +15,31 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseDatabase database;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        database = FirebaseDatabase.getInstance();
-        final TextView emailTextView = (TextView)findViewById(R.id.textView7);
-        final TextView passTextView = (TextView)findViewById(R.id.textView9);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        final TextView emailTextView = (TextView) findViewById(R.id.textView7);
+        final TextView passTextView = (TextView) findViewById(R.id.textView9);
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = ((EditText)findViewById(R.id.editText)).getText().toString();
-                String pass = ((EditText)findViewById(R.id.editText2)).getText().toString();
+                String email = ((EditText) findViewById(R.id.editText)).getText().toString();
+                String pass = ((EditText) findViewById(R.id.editText2)).getText().toString();
 
                 submit(email, pass);
             }
         });
 
-        database.getReference("account").addValueEventListener(new ValueEventListener() {
+        mDatabase.child("account").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                HashMap<String, String> account = (HashMap<String, String>)dataSnapshot.getValue();
+                HashMap<String, String> account = (HashMap<String, String>) dataSnapshot.getValue();
                 emailTextView.setText(account.get("email"));
                 passTextView.setText(account.get("pass"));
             }
@@ -51,12 +51,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void submit(String email, String pass){
+    private void submit(String email, String pass) {
         HashMap<String, String> account = new HashMap<>();
         account.put("email", email);
         account.put("pass", pass);
 
-        DatabaseReference refAccount = database.getReference("account");
-        refAccount.setValue(account);
+        mDatabase.child("account").setValue(account);
     }
 }

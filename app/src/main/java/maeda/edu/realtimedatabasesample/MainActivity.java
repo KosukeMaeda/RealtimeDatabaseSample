ffcac5486759caa0b9dd413027ec1e36414878eb
Kosuke Maeda <maeda.korosuke@gmail.com>
2016-10-06 12:13:36 +0900

    データを追加できるように変更した。

--- app/src/main/java/maeda/edu/realtimedatabasesample/MainActivity.java
+++ app/src/main/java/maeda/edu/realtimedatabasesample/MainActivity.java
@@ -2,17 +2,24 @@ package maeda.edu.realtimedatabasesample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
@@ -23,9 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        final TextView emailTextView = (TextView) findViewById(R.id.textView7);
-        final TextView passTextView = (TextView) findViewById(R.id.textView9);
-
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,12 +40,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mDatabase.child("account").addValueEventListener(new ValueEventListener() {
+
+        final Query query = mDatabase.child("account").orderByKey();
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                HashMap<String, String> account = (HashMap<String, String>) dataSnapshot.getValue();
-                emailTextView.setText(account.get("email"));
-                passTextView.setText(account.get("pass"));
+                String s = "";
+                for(DataSnapshot child : dataSnapshot.getChildren()){
+                    String key = child.getKey();
+                    String account = child.getValue().toString();
+                    Log.d("data", key +  account);
+                    s += account + "\n";
+                }
+                ((TextView)findViewById(R.id.textView3)).setText(s);
             }
 
             @Override
@@ -49,13 +60,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
     private void submit(String email, String pass) {
         HashMap<String, String> account = new HashMap<>();
         account.put("email", email);
         account.put("pass", pass);
-
-        mDatabase.child("account").setValue(account);
+        String key = mDatabase.child("account").push().getKey();
+        mDatabase.child("account").child(key).setValue(account);
     }
 }
